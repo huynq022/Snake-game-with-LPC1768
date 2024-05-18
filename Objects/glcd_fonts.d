034eedbe40c91c1e4d9ca846f06cd790ea5aa91c
huynq022 <ititiu20214@student.hcmiu.edu.vn>
2024-05-18 12:20:31 +0700

    add main.c

---
+++ Objects/glcd_fonts.d
@@ -0,0 +1,6 @@
+./objects/glcd_fonts.o: \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Boards\Keil\MCB1700\Common\GLCD_Fonts.c \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Boards\Keil\MCB1700\Common\GLCD_Fonts.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_GLCD.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h
---
+++ Objects/main.d
@@ -0,0 +1,19 @@
+./objects/main.o: main.c \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\LPC17xx.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\RTE_Driver\PIN_LPC17xx.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\RTE_Driver\GPIO_LPC17xx.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Boards\Keil\MCB1700\Common\GLCD_Config.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\MDK-Middleware\7.16.0\Board\Board_GLCD.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Boards\Keil\MCB1700\Common\GLCD_Fonts.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_SPI.h \
+  C:\Users\PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_Common.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
